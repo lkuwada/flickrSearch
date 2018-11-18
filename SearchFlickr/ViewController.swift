a51d2e8e387c0881f0fcdebe9c6266603a8e268f
Lynden Kuwada <Lynden@MacBook-Pro.local>
2018-11-17 23:43:42 -0800

    Initial Commit

---
+++ SearchFlickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchFlickr
+//
+//  Created by Lynden Kuwada on 11/17/18.
+//  Copyright © 2018 Lynden Kuwada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
